de2e1437f88328a02bf8e0a6b4d31f8149d7c55a
Luz Cazares <luz.cazares@intel.com>
2017-11-08 17:50:08 -0800

    Fixing travis1
    Just to fix travis ci

--- createCN.sh
+++ createCN.sh
@@ -11,3 +11,4 @@ exit 1
 for i in `seq 1 n`; do
     createvm --name "centos-CN0${i}" --ostype Linux --register
     vboxmanage modifyvm  "centos-CN0${i}" --memory 1024 --boot1 net --chipset piix3 --nic1 intnet --nictype1 82540EM --nicpromisc1 deny --cableconnected1 on --macaddress1 0800277CF270
+done
